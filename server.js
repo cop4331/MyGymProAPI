3cb90fd5efa8a65e74b2cfab13d04a689dfa27c2
cop4331 <62314583+cop4331@users.noreply.github.com>
2020-04-16 13:55:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var MongoClient = require('mongodb').MongoClient;
 
 var bcrypt = require('bcrypt');
 
+var nodemailer = require('nodemailer');
+
 var app = express();
 
 var conn = 'mongodb+srv://mainaccess:securepassword@cop4331-large-project-l2dqk.mongodb.net/MyGymPro?retryWrites=true&w=majority';
@@ -49,6 +51,34 @@ app.post('/api/signup', async (req, res) =>
     }
 	  
     const result = db.collection('Users').insertOne(newUser);
+	  
+    var smtpTransport = nodemailer.createTransport({
+	    service: "Gmail",
+	    auth: {user: "mygymproapp@gmail.com", pass: "Exceptions123?"}
+    });
+
+    var rand, mailOptions, host, link;
+	  
+    app.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+      });
+    });
   }
   catch(e)
   {
