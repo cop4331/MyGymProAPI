292ab1d51b2bf2665417167b5487cc2909fa2636
cop4331 <62314583+cop4331@users.noreply.github.com>
2020-04-16 14:03:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,34 +51,6 @@ app.post('/api/signup', async (req, res) =>
     }
 	  
     const result = db.collection('Users').insertOne(newUser);
-	  
-    var smtpTransport = nodemailer.createTransport({
-	    service: "Gmail",
-	    auth: {user: "mygymproapp@gmail.com", pass: "Exceptions123?"}
-    });
-
-    var rand, mailOptions, host, link;
-	  
-    app.get('/send',function(req,res){
-        rand=Math.floor((Math.random() * 100) + 54);
-    host=req.get('host');
-    link="http://"+req.get('host')+"/verify?id="+rand;
-    mailOptions={
-        to : req.query.to,
-        subject : "Please confirm your Email account",
-        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-      });
-    });
   }
   catch(e)
   {
@@ -86,6 +58,40 @@ app.post('/api/signup', async (req, res) =>
   }
   
   res.status(200).json({Error:error});
+});
+
+var smtpTransport = nodemailer.createTransport(
+{
+	    service: "Gmail",
+	    auth: {user: "mygymproapp@gmail.com", pass: "Exceptions123?"}
+});
+
+var rand, mailOptions, host, link;
+	  
+app.get('/api/sendverificationemail', async (req,res) =>
+{
+  rand=Math.floor((Math.random() * 100) + 54);
+  host=req.get('host');
+  link="http://"+req.get('host')+"/verify?id="+rand;
+  mailOptions = 
+  {
+    to : req.query.to,
+    subject : "Please confirm your Email account",
+    html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+  }
+  console.log(mailOptions);
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if (error)
+    {
+      console.log(error);
+      res.end("error");
+    }
+    else
+    {
+      console.log("Message sent: " + response.message);
+      res.end("sent");
+    }
+  });
 });
 
 app.post('/api/createpost', async (req, res) =>
